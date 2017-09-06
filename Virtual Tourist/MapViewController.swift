d3ab71e07d82ce1e8bd6072aecbd3f055baea159
Leonardo Simas <leosimasgoncalves@gmail.com>
2017-09-05 22:20:18 -0300

    project creation with mapviewcontroller

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by SoSucesso on 05/09/17.
+//  Copyright © 2017 Leonardo Simas. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+
+}
+
