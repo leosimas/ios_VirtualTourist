660a80109a18857f873ac16908bba45725f2a585
Leonardo Simas <leosimasgoncalves@gmail.com>
2017-09-07 08:32:53 -0300

    adding animated pins to the mapview

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -7,9 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView : MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(onMapLongPress)))
+    }
+    
+    func onMapLongPress(gesture : UIGestureRecognizer) {
+        if gesture.state != .began {
+            return
+        }
+        
+        let viewLocation = gesture.location(in: mapView)
+        let coordinate = mapView.convert(viewLocation, toCoordinateFrom: mapView)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        
+        mapView.addAnnotation(pin)
+    }
 
+}
+
+extension MapViewController : MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let id = "pinView"
+        var view = mapView.dequeueReusableAnnotationView(withIdentifier: id) as? MKPinAnnotationView
+        if(view == nil) {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            view!.animatesDrop = true
+        }
+        return view
+    }
+    
 }
 
